f6ea1dcbc3acf5908360a4f563f5b82c3e59d151
idiosyncratic16 <vedantpathak622@gmail.com>
2026-06-04 00:34:40 +0530

    revert supabase connection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+<html lang="en" className="dark">
       <body className={`${dmSans.variable} font-sans`}>{children}</body>
     </html>
   );
